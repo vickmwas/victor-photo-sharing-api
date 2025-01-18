c5a7cc4ca3b5c2615eac7baf3b568e602182a954
Victor Mwangi <vickmwas@gmail.com>
2025-01-18 18:10:27 +0300

    Add Comments Module and CRUD endpoints

---
+++ src/comments/entities/comment.entity.ts
@@ -0,0 +1,35 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+} from 'typeorm';
+import { Photo } from '../../photos/entities/photo.entity';
+import { User } from '../../users/entities/user.entity';
+
+@Entity('comments')
+export class Comment {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('text')
+  comment: string;
+
+  @ManyToOne(() => Photo, (photo) => photo.id)
+  photoId: number;
+
+  @ManyToOne(() => User, (user) => user.id)
+  userId: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+}
