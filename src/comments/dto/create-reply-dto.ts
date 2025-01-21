e46efb5baf3bf2fa98211cc592ae613b7a85cb7b
Victor Mwangi <vickmwas@gmail.com>
2025-01-21 06:11:22 +0300

    Refactor by removing unused comment reply DTO file

--- src/comments/dto/create-reply-dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsString, IsNotEmpty  } from 'class-validator';
-
-export class CreateReplyDto {
-  @ApiProperty({ example: 'This is a reply to the comment' })
-  @IsString()
-  @IsNotEmpty()
-  text: string;
-}
