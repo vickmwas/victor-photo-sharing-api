93f62559a3df66ba8a20bcd24e858b47a4d6ff42
Victor Mwangi <vickmwas@gmail.com>
2025-01-23 00:32:25 +0300

    run eslint formatting

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -1,4 +1,4 @@
-import {  Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { NotificationsController } from './notifications.controller';
 import { NotificationsService } from './notifications.service';
